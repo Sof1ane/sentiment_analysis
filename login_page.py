de473dc7b10b41b1488f3014ddba2c5706dc2b4e
Sof1ane <43276871+Sof1ane@users.noreply.github.com>
2022-05-11 14:17:31 +0200

    Add files via upload

--- login_page.py
+++ login_page.py
@@ -21,37 +21,36 @@ conn = sqlite3.connect('data.db')
 c = conn.cursor()
 # DB  Functions
 
-def form():
+def form(username):
     st.write("Enter your feelings")
     with st.form(key = "information form"):
-        name = st.text_input("Enter your name")
-        first_name = st.text_input("Enter your first name")
+        
         date = st.date_input("Enter the date : ")
         thoughts = st.text_area("Write what you have to say ")
         submission = st.form_submit_button(label = "submit")
 
         if submission == True:
-            x= requests.post('http://127.0.0.1:5000/predict', json={"thoughts": thoughts})
+            x= requests.post('https://apiprediction.azurewebsites.net/predict', json={"thoughts": thoughts})
             prediction = x.text
             print(prediction)
             sentiment = prediction            
-            add_data(name, first_name, date,thoughts, sentiment)
+            add_data(username,date,thoughts, sentiment)
             
             
             
-def add_data(a,b,h,d,e):
-    c.execute("""CREATE TABLE IF NOT EXISTS thoughts_form(NAME TEXT(50), FIRSTNAME TEXT(50), DATE TEXT(50), THOUGHTS TEXT(500), SENTIMENT TEXT(50));""")
-    c.execute("INSERT INTO thoughts_form VALUES (?,?,?,?,?)", (a,b,h,d,e))
+def add_data(a,b,h,d):
+    c.execute("""CREATE TABLE IF NOT EXISTS thoughts_form(USERNAME TEXT(50), DATE TEXT(50), THOUGHTS TEXT(500), SENTIMENT TEXT(50));""")
+    c.execute("INSERT INTO thoughts_form VALUES (?,?,?,?)", (a,b,h,d))
     conn.commit()
     conn.close()
     st.success("Successfully added")
     
 def create_usertable():
-	c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT,password TEXT)')
+	c.execute('CREATE TABLE IF NOT EXISTS userstable(NAME TEXT(50), FIRSTNAME TEXT(50),username TEXT,password TEXT, status TEXT)')
 
 
-def add_userdata(username,password):
-	c.execute('INSERT INTO userstable(username,password) VALUES (?,?)',(username,password))
+def add_userdata(name,firstname,username,password):
+	c.execute('INSERT INTO userstable(name,firstname,username,password, status) VALUES (?,?,?,?,?)',(name,firstname,username,password, "patient"))
 	conn.commit()
 
 def login_user(username,password):
@@ -59,59 +58,79 @@ def login_user(username,password):
 	data = c.fetchall()
 	return data
 
+def check_status(username):
+	status = c.execute('SELECT status FROM userstable WHERE username=?',(username,))
+	status = status.fetchone()[0]
+	return status
 
 def view_all_users():
 	c.execute('SELECT * FROM userstable')
 	data = c.fetchall()
 	return data
 
+def view_all_thoughts():
+	c.execute('SELECT * FROM thoughts_form')
+	data = c.fetchall()
+	return data
+
 
 
 def main():
-	"""Simple Login App"""
+	"""Write about your feelings"""
 
-	st.title("Simple Login App")
+	st.title("User page")
 
 	menu = ["Login","SignUp"]
 	choice = st.sidebar.selectbox("Menu",menu)
  
 	if choice == "Login":
-		st.subheader("Login Section")
+		st.subheader("View your account information")
 
 		username = st.sidebar.text_input("User Name")
 		password = st.sidebar.text_input("Password",type='password')
 		if st.sidebar.checkbox("Login"):
-			# if password == '12345':
+
 			create_usertable()
 			hashed_pswd = make_hashes(password)
 
 			result = login_user(username,check_hashes(password,hashed_pswd))
 			if result:
-				st.success("Logged In as {}".format(username))
-    
-
-				task = st.selectbox("Task",["Add Post","Analytics","Profiles"])
-				if task == "Add Post":
-					form()
-
-				elif task == "Analytics":
-					st.subheader("Analytics")
-				elif task == "Profiles":
-					st.subheader("User Profiles")
-					user_result = view_all_users()
-					clean_db = pd.DataFrame(user_result,columns=["Username","Password"])
-					st.dataframe(clean_db)
+				
+				if check_status(username) == "patient":
+					st.success("Logged In as {}".format(username))
+		
+					form(username)
+		
+				elif check_status(username) == "doctor":
+					task = st.selectbox("Task",["Analytics","Profiles", "Thoughts"])
+					if task == "Analytics":
+						st.subheader("Analytics")
+      
+					elif task == "Profiles":
+						st.subheader("User Profiles")
+						user_table = view_all_users()
+						clean_db_1 = pd.DataFrame(user_table,columns=["name", "firstname","Username","Password","Status"])
+						st.dataframe(clean_db_1)
+      
+					elif task == "Thoughts":
+						st.subheader("User Thoughts")
+						user_thoughts = view_all_thoughts()
+						clean_db_2 = pd.DataFrame(user_thoughts,columns=["Username", "Date","Thoughts","Sentiment"])
+						st.dataframe(clean_db_2)
+					
 			else:
 				st.warning("Incorrect Username/Password")
     
 	elif choice == "SignUp":
 		st.subheader("Create an Account")
+		new_name = st.text_input("Enter your name")
+		new_first_name = st.text_input("Enter your first name")
 		new_user = st.text_input('Username')
 		new_passwd = st.text_input('Password',type='password')
   
 		if st.button('SignUp'):
 			create_usertable()
-			add_userdata(new_user,make_hashes(new_passwd))
+			add_userdata(new_name, new_first_name, new_user,make_hashes(new_passwd))
 			st.success("You have successfully created an account.Go to the Login Menu to login")
 
 
