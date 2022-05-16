66b0246eae7f2a36b98f82d15c09b7ce78e8718e
Sof1ane <43276871+Sof1ane@users.noreply.github.com>
2022-05-16 15:08:19 +0200

    Add files via upload

--- login_page.py
+++ login_page.py
@@ -1,5 +1,3 @@
-# Security
-#passlib,hashlib,bcrypt,scrypt
 import streamlit as st
 import hashlib
 import pandas as pd
@@ -188,6 +186,27 @@ def main():
 
 								else:
 									c.execute("SELECT username,sentiment,count(sentiment) FROM thoughts_form WHERE username = ? AND date between ? and ? GROUP BY sentiment",(wheel_username,first_date,second_date))
+									thoughts_year = c.fetchall()
+									between_year_df = pd.DataFrame(thoughts_year, columns=["username","sentiment","count"])
+									fig = px.pie(between_year_df, values = "count",names="sentiment" )
+									st.plotly_chart(fig, use_container_width=True)
+         
+						elif user_or_name == "Name and First Name":
+							wheel_name = st.text_input('Show wheel for this user')
+							wheel_first_name = st.text_input('Show wheel for this user')
+							first_date = st.date_input('Chose first date')
+							second_date = st.date_input('Chose second date')
+							submit_year = st.button(label = "Submit year")
+							if submit_year:
+								if first_date == second_date :
+									c.execute("SELECT username,sentiment,count(sentiment) FROM thoughts_form WHERE name=? and firstname=? AND date = ? GROUP BY sentiment",(wheel_name,wheel_first_name,first_date))
+									thoughts_year = c.fetchall()
+									between_year_df = pd.DataFrame(thoughts_year, columns=["username","sentiment","count"])
+									fig = px.pie(between_year_df, values = "count",names="sentiment" )
+									st.plotly_chart(fig, use_container_width=True)
+
+								else:
+									c.execute("SELECT username,sentiment,count(sentiment) FROM thoughts_form WHERE name=? and firstname=? AND date between ? and ? GROUP BY sentiment",(wheel_name,wheel_first_name,first_date,second_date))
 									thoughts_year = c.fetchall()
 									between_year_df = pd.DataFrame(thoughts_year, columns=["username","sentiment","count"])
 									fig = px.pie(between_year_df, values = "count",names="sentiment" )
